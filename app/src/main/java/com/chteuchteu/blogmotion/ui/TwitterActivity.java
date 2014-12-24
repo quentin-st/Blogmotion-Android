6a879e4ed9d71277503677c8ee7f01edb7f2b1d2
Quentin S <chteuchteu@gmail.com>
2014-12-24 15:30:24 +0100

    Refresh button on Twitter page

--- app/src/main/java/com/chteuchteu/blogmotion/ui/TwitterActivity.java
+++ app/src/main/java/com/chteuchteu/blogmotion/ui/TwitterActivity.java
@@ -3,6 +3,7 @@ package com.chteuchteu.blogmotion.ui;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 import com.chteuchteu.blogmotion.R;
@@ -10,6 +11,9 @@ import com.chteuchteu.blogmotion.hlpr.Util;
 
 public class TwitterActivity extends BMActivity {
 	private static final String TWITTER_URL = "http://blogmotion.fr/public/android/m.twitter/";
+
+    private WebView webView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,7 +22,7 @@ public class TwitterActivity extends BMActivity {
 		this.menuRes = R.menu.menu_twitter;
 		this.currentActivity = TwitterActivity.class;
 
-		WebView webView = (WebView) findViewById(R.id.wv);
+		webView = (WebView) findViewById(R.id.wv);
 		webView.loadUrl(TWITTER_URL);
 		webView.getSettings().setDomStorageEnabled(true);
 		webView.getSettings().setJavaScriptEnabled(true);
@@ -26,6 +30,16 @@ public class TwitterActivity extends BMActivity {
 
 		super.afterOnCreate();
 	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                webView.reload();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 	@Override
 	public void onBackPressed() {
